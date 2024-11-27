4df2af5d6a34526c15f0f7400722f9f0e1fa86a1
thisangaThewanmi <thisangat2005@gmail.com>
2024-11-27 21:56:33 +0530

    did some changes in staff and Equipment Forms

---
+++ script.js
@@ -0,0 +1,17 @@
+function updateTime() {
+    const timeElement = document.getElementById('current-time');
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    timeElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+// Update the time every second
+setInterval(updateTime, 1000);
+
+// Set the initial time
+updateTime();
+
+
+/*-=================*/
